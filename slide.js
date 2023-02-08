915857fa4423b3ced1aa5c98dc0e66e2d06e108f
luquinhaspmw <luquinhaspmw@gmail.com>
2023-02-08 20:25:24 -0300

    teste6

--- slide.js
+++ slide.js
@@ -8,18 +8,19 @@ slideshow.addEventListener("mouseout",()=>{
     listaSlideShow.style.animationPlayState = "running";
 })
 
-let px = window.innerWidth+"px";
-document.documentElement.style.setProperty("--screenActive", px)
+
 setInterval(()=>{
-    document.body.onresize = ()=>{
-        px = window.innerWidth+"px";
-        document.documentElement.style.setProperty("--screenActive", px)
+    // document.body.onresize = ()=>{
+    //     let px = window.innerWidth+"px";
+    //     document.documentElement.style.setProperty("--screenActive", px)
     
-        if(window.innerWidth > 700 && window.innerWidth < 900){
-            slideshow.classList.add("active-grid");
-        }else{
-            slideshow.classList.remove("active-grid");
-        }
-    }
+    //     if(window.innerWidth > 700 && window.innerWidth < 900){
+    //         slideshow.classList.add("active-grid");
+    //     }else{
+    //         slideshow.classList.remove("active-grid");
+    //     }
+    // }
+    let px = window.innerWidth+"px";
+    document.documentElement.style.setProperty("--screenActive", px)
 },1)
 
